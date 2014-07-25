0904fb065834abce3721eeb3a12255cfdb08fc53
JasonDevStudio <ylsoo@ylsoo.net>
2014-07-25 17:17:02 +0800

    WCF项目

---
+++ WCFSolution/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Uri[] baseAddresses = new Uri[] { 
+                  new Uri("net.tcp://localhost:8734/Design_Time_Addresses/ConsoleApplication1/Service1/")
+              };
+                ServiceHost host = new ServiceHost(typeof(Service1));
+                ServiceHost host1 = new ServiceHost(typeof(Service2));
+                host1.Open();
+                host.Open();
+
+                Console.WriteLine("服务已启动！");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("服务启动失败："+ex.Message);
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
---
+++ WCFSolution/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.IService1 isc = new ServiceReference1.Service1Client("NetTcpBinding_IService1");
+            var msg =isc.DoWork(new ClassLibrary1.ClassModel(),"测试结果:OK");
+            MessageBox.Show(msg);
+        }
+    }
+}
